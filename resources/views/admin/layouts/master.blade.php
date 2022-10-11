d49d207c323844961a620927bca8ef186bb87d76
AungKyawPhyo1142 <aungkyawphyo22233@gmail.com>
2022-10-11 21:07:29 +0630

    Changing into Template Instead of the scrappy UI, changing into Template

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,40 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Project Media</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Project Media</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 </head>
-<body>
-    <div class=" p-2">
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+  </nav>
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="#" class="brand-link">
 
-        <div class="row">
+      <span class="brand-text font-weight-light">Project Media</span>
+    </a>
+    <div class="sidebar">
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-            {{-- side bar --}}
-            <div class="col-3 text-center">
-                <a href="{{route('dashboard')}}" class="btn btn-dark text-white w-100 my-2"><i class="fa-solid fa-user me-2"></i>Profile</a>
-                <a href="{{route('admin#list')}}" class="btn btn-dark text-white w-100 my-2"><i class="fa-solid fa-users me-2"></i>Admin List</a>
-                <a href="{{route('admin#category')}}" class="btn btn-dark text-white w-100 my-2"><i class="fa-solid fa-list me-2"></i>Category List</a>
-                <a href="{{route('admin#post')}}" class="btn btn-dark text-white w-100 my-2"><i class="fa-solid fa-newspaper me-2"></i>Posts</a>
-                <a href="{{route('admin#trendPost')}}" class="btn btn-dark text-white w-100 my-2"><i class="fa-solid fa-arrow-trend-up me-2"></i>Trending Posts</a>
+          <li class="nav-item my-2">
+            <a href="{{route('dashboard')}}" class="nav-link btn btn-dark text-white">
+              <i class="fas fa-user-circle"></i>
+              <p>
+                My Profile
+              </p>
+            </a>
+          </li>
 
-                <form action="{{route('logout')}}" method="post">
-                    @csrf
-                    <button class="btn btn-dark text-white w-100 my-2" type="submit"><i class="fa-solid fa-right-from-bracket me-2"></i>Logout</button>
-                </form>
+          <li class="nav-item my-2">
+            <a href="{{route('admin#list')}}" class="nav-link btn btn-dark text-white">
+                <i class="fa-solid fa-users"></i>
+              <p>
+                Admin List
+              </p>
+            </a>
+          </li>
 
-            </div>
+          <li class="nav-item my-2">
+            <a href="{{route('admin#category')}}" class="nav-link btn btn-dark text-white">
+              <i class="fas fa-list"></i>
+              <p>
+                Category
+              </p>
+            </a>
+          </li>
 
-            {{-- content --}}
-            <div class="col">
-                @yield('content')
-            </div>
+          <li class="nav-item my-2">
+            <a href="{{route('admin#post')}}" class="nav-link btn btn-dark text-white">
+                <i class="fa-solid fa-newspaper"></i>
+              <p>
+                Posts
+              </p>
+            </a>
+          </li>
 
-        </div>
+         <li class="nav-item my-2">
+            <a href="{{route('admin#trendPost')}}" class="nav-link btn btn-dark text-white">
+                <i class="fa-solid fa-arrow-trend-up"></i>
+              <p>
+                Trending Posts
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item my-2">
+            <form action="{{route('logout')}}" method="POST">
+                @csrf
+                    <button type="submit" class="nav-link btn btn-dark text-white">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <p>Logout</p>
+                    </button>
+            </form>
+          </li>
+        </ul>
+      </nav>
     </div>
+  </aside>
+
+  <div class="content-wrapper">
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row mt-4">
+            @yield('content')
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <aside class="control-sidebar control-sidebar-dark">
+  </aside>
+</div>
+<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('dist/js/demo.js')}}"></script>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -1,34 +1,68 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <form action="">
-        <div class="row p-2 align-items-center">
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" placeholder="Enter your name...">
+
+<div class="col-8 offset-3 mt-5">
+    <div class="col-md-9">
+      <div class="card">
+        <div class="card-header p-2">
+          <legend class="text-center">User Profile</legend>
+        </div>
+
+        <div class="card-body">
+          <div class="tab-content">
+            <div class="active tab-pane" id="activity">
+              <form class="form-horizontal">
+
+                <div class="form-group row">
+                  <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputName" placeholder="Name">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="gender" class="col-sm-2 col-form-label">Gender</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="gender" placeholder="Gender">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="phone" class="col-sm-2 col-form-label">Phone</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="phone" placeholder="Phone">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="address" class="col-sm-2 col-form-label">Address</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="address" placeholder="Address">
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-sm-10 offset-sm-2 d-flex align-items-center justify-content-between">
+                        <button class="btn btn-primary">Submit</button>
+                        <a class="text-muted">Change Password</a>
+                    </div>
+                </div>
+
+            </form>
+
             </div>
-        </div>
-        <div class="row p-2 align-items-center">
-            <div class="mb-3">
-                <label for="email" class="form-label">Email:</label>
-                <input type="text" class="form-control" id="email" placeholder="Enter your email...">
             </div>
+          </div>
         </div>
-        <div class="row p-2 align-items-center">
-            <div class="mb-3">
-                <label for="phone" class="form-label">Phone</label>
-                <input type="text" class="form-control" id="phone" placeholder="Enter your phone number...">
-            </div>
-        </div>
-        <div class="row p-2 align-items-center">
-            <div class="mb-3">
-                <label for="address" class="form-label">Address:</label>
-                <textarea name="address" id="" cols="20" rows="5" class="form-control" placeholder="Enter your address..."></textarea>
-            </div>
-        </div>
-        <div class="row pt-2 pb-3 px-4 align-items-center justify-content-between">
-            <button class="btn btn-primary col-3"><i class="fa-solid fa-pen-to-square me-2"></i>Update</button>
-            <button class="btn btn-secondary col-3"><i class="fa-solid fa-key me-2"></i>Change Password </button>
-        </div>
-    </form>
+      </div>
+    </div>
+
 @endsection
