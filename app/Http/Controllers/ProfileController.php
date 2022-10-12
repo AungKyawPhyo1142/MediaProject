d8cd58bd7355a6b28adac43fba55dfd08280d878
AungKyawPhyo1142 <aungkyawphyo22233@gmail.com>
2022-10-12 12:01:04 +0630

    Get data for admin profile Get data from database for the admin profile when the page is loaded

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     // go to admin dashboard
     public function goDashboard(){
-        return view('admin.profile.index');
+        $currentID = Auth::user()->id;
+        $currentProfileData = User::select('id','name','phone','address','gender','email')
+                                ->where('id',$currentID)->first();
+        return view('admin.profile.index',compact('currentProfileData'));
     }
 }
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -17,41 +17,45 @@
                 <div class="form-group row">
                   <label for="inputName" class="col-sm-2 col-form-label">Name</label>
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputName" placeholder="Name">
+                    <input type="text" class="form-control" id="inputName" placeholder="Enter name..." value="{{$currentProfileData->name}}">
                   </div>
                 </div>
 
                 <div class="form-group row">
                   <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Enter email address..." value="{{$currentProfileData->email}}">
                   </div>
                 </div>
 
                 <div class="form-group row">
                   <label for="gender" class="col-sm-2 col-form-label">Gender</label>
                   <div class="col-sm-10">
-                    <input type="text" class="form-control" id="gender" placeholder="Gender">
+                    <select name="gender" class="form-control" id="gender">
+                        <option value="" @if($currentProfileData->gender=='') selected @endif>Choose your gender</option>
+                        <option value="male" @if($currentProfileData->gender=='male') selected @endif>Male</option>
+                        <option value="female" @if($currentProfileData->gender=='female') selected @endif>Female</option>
+                    </select>
                   </div>
                 </div>
 
                 <div class="form-group row">
                   <label for="phone" class="col-sm-2 col-form-label">Phone</label>
                   <div class="col-sm-10">
-                    <input type="text" class="form-control" id="phone" placeholder="Phone">
+                    <input type="text" class="form-control" id="phone" placeholder="Enter phone..." value="{{$currentProfileData->phone}}">
                   </div>
                 </div>
 
                 <div class="form-group row">
                   <label for="address" class="col-sm-2 col-form-label">Address</label>
                   <div class="col-sm-10">
-                    <input type="text" class="form-control" id="address" placeholder="Address">
+                    <textarea name="address" class="form-control" id="address" cols="30" rows="5" placeholder="Enter your address...">{{$currentProfileData->address}}</textarea>
                   </div>
                 </div>
 
                 <div class="form-group row">
                     <div class="col-sm-10 offset-sm-2 d-flex align-items-center justify-content-between">
-                        <button class="btn btn-primary">Submit</button>
+                        <button class="btn btn-primary">Update</button>
                         <a class="text-muted">Change Password</a>
                     </div>
                 </div>
